b1e145cbd01fd69c1f61c6baae4e0b4e19f81ac5
Adriana Ivich <108026185+ivichadriana@users.noreply.github.com>
2023-01-18 12:49:38 -0700

    Update and rename pbmc_diva_test.py to pbmc_cortex_test.py

--- evaluation_experiments/pbmc/pbmc_cortex_test.py
+++ evaluation_experiments/pbmc/pbmc_cortex_test.py
@@ -65,62 +65,62 @@ if __name__ == "__main__":
 
     # read in the test / train data
     _, Y_train, _, _ = sc_preprocess.read_diva_files(args.aug_data_path, 6, args.train_id, use_test=False)
-    X_pbmc2, Y_pbmc2, pbmc2_gene_df, _ = sc_preprocess.read_diva_files(args.aug_data_path, 6, args.test_id, use_test=False)
+    X_cortex2, Y_cortex2, cortex2_gene_df, _ = sc_preprocess.read_diva_files(args.aug_data_path, 6, args.test_id, use_test=False)
     gene_file = os.path.join(args.res_data_path, f"train-{args.train_id}-{args.unlab_exp_id}-DIVA_features.pkl")
     gene_path = Path(gene_file)
     gene_df_train = pickle.load( open( gene_path, "rb" ) )
 
     print(f"Encoder files: {args.res_data_path}/{args.train_id}_{args.unlab_exp_id}_encoder")
-    print(f"X shape: {X_pbmc2.shape}")
-    print(f"Y shape: {Y_pbmc2.shape}")
+    print(f"X shape: {X_cortex2.shape}")
+    print(f"Y shape:  {Y_cortex2.shape}")
     print(f"Y_train shape: {Y_train.shape}")
 
-    #X_pbmc2 = pd.concat([X_pbmc2, X_pbmc2, X_pbmc2, X_pbmc2, X_pbmc2])
-    #Y_pbmc2 = pd.concat([Y_pbmc2, Y_pbmc2, Y_pbmc2, Y_pbmc2, Y_pbmc2])
+    #X_cortex2 = pd.concat([X_cortex2, X_cortex2, X_cortex2, X_cortex2, X_cortex2])
+    #Y_cortex2 = pd.concat([Y_cortex2, Y_cortex2, Y_cortex2, Y_cortex2, Y_cortex2])
 
-    print(f"X shape: {X_pbmc2.shape}")
-    print(f"Y shape: {Y_pbmc2.shape}")
+    print(f"X shape: {X_cortex2.shape}")
+    print(f"Y shape:  {Y_cortex2.shape}")
     print(f"Y_train shape: {Y_train.shape}")
 
 
     # now we need to ensure the genes are in the same order
-    X_pbmc2.columns = pbmc2_gene_df
-    X_pbmc2 = X_pbmc2.reindex(columns=gene_df_train, fill_value=0)
-    X_pbmc2 = X_pbmc2[gene_df_train]
+    X_cortex2.columns = cortex2_gene_df
+    X_cortex2 = X_cortex2.reindex(columns=gene_df_train, fill_value=0)
+    X_cortex2 = X_cortex2[gene_df_train]
 
     # we also need to ensure that the cell-types are in the same order
     ## need to add somthing to fill in missing columns
-    Y_pbmc2 = Y_pbmc2.reindex(columns=Y_train.columns, fill_value=0)
-    Y_pbmc2 = Y_pbmc2[Y_train.columns]
+    Y_cortex2 = Y_cortex2.reindex(columns=Y_train.columns, fill_value=0)
+    Y_cortex2 = Y_cortex2[Y_train.columns]
 
     # cast from DF to NPArray
-    X_pbmc2 = X_pbmc2.to_numpy()
-    Y_pbmc2 = Y_pbmc2.to_numpy()
+    X_cortex2 = X_cortex2.to_numpy()
+    Y_cortex2 = Y_cortex2.to_numpy()
 
     # normalilze within sample
-    X_pbmc2 = scale(X_pbmc2, axis=1)
-    Y_pbmc2 = Y_pbmc2/Y_pbmc2.sum(axis=1,keepdims=1)
+    X_cortex2 = scale(X_cortex2, axis=1)
+    Y_cortex2 = Y_cortex2/Y_cortex2.sum(axis=1,keepdims=1)
 
     # now run it
     batch_size = 500
 
-    z_slack, mu_slack, l_sigma_slack, mu_prop, l_sigma_prop, prop_outputs, z_rot, mu_rot, l_sigma_rot, z_drug, mu_drug, l_sigma_drug = encoder.predict(X_pbmc2, batch_size=batch_size)
+    z_slack, mu_slack, l_sigma_slack, mu_prop, l_sigma_prop, prop_outputs, z_rot, mu_rot, l_sigma_rot= encoder.predict(X_cortex2, batch_size=batch_size)
     z_test = prop_outputs
     encodings = np.asarray(z_test)
-    #encodings = encodings.reshape(X_pbmc2.shape[0], n_z)
+    #encodings = encodings.reshape(X_cortex2.shape[0], n_z)
 
 
-    test_error = [mean_sqr_error(Y_pbmc2[idx], encodings[idx]) 
-                    for idx in range(0, X_pbmc2.shape[0])]
+    test_error = [mean_sqr_error(Y_cortex2[idx], encodings[idx]) 
+                    for idx in range(0, X_cortex2.shape[0])]
 
     print(f"MSqE mean: {np.mean(test_error)}, median: {np.median(test_error)}, max: {max(test_error)}")
 
-    test_error = [spearmanr(Y_pbmc2[idx].astype(float), encodings[idx].astype(float))[0]
-                    for idx in range(0, X_pbmc2.shape[0])]
+    test_error = [spearmanr(Y_cortex2[idx].astype(float), encodings[idx].astype(float))[0]
+                    for idx in range(0, X_cortex2.shape[0])]
     print(f"Spearman mean: {np.mean(test_error)}, median: {np.median(test_error)}, max: {max(test_error)}")
 
-    test_error = [pearsonr(Y_pbmc2[idx].astype(float), encodings[idx].astype(float))[0]
-                    for idx in range(0, X_pbmc2.shape[0])]
+    test_error = [pearsonr(Y_cortex2[idx].astype(float), encodings[idx].astype(float))[0]
+                    for idx in range(0, X_cortex2.shape[0])]
     print(f"Pearson mean: {np.mean(test_error)}, median: {np.median(test_error)}, max: {max(test_error)}")
 
     # write out the result
@@ -129,4 +129,5 @@ if __name__ == "__main__":
 
     res_file = os.path.join(args.res_data_path, f"train-{args.train_id}-test-{args.test_id}-unlab-{args.unlab_exp_id}-DIVA.pkl")
     res.to_pickle(res_file)
+    
         
